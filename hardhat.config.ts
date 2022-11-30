1d1642b4c77b5221e8e5c8afe833b12d0185acfe
Kennedy Eruba <erubakenny@gmail.com>
2022-11-30 12:51:19 +0000

    removed env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,26 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { TESTNET_PRIVATE_KEY, TESTNET_NODE_URL } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: TESTNET_NODE_URL,
+      accounts: [`0x${TESTNET_PRIVATE_KEY}`]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
 
 export default config;
